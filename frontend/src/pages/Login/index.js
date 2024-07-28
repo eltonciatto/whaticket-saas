578fda439b426ce784889575ab7b86c908d9332f
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-07-27 22:57:58 -0300

    Create index.js

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,145 @@
+import React, { useState, useContext } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { versionSystem, nomeEmpresa } from "../../../package.json";
+import { i18n } from "../../translate/i18n";
+import { AuthContext } from "../../context/Auth/AuthContext";
+import "../../assets/ReactToastify-BTGsrsBX.css"; // Importe o arquivo CSS
+import logo from "../../assets/logo.png";
+
+// Componente de copyright
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="primary" align="center">
+      {"Copyright "}
+      <Link color="primary" href="#">
+        {nomeEmpresa} - v {versionSystem}
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+// Estilos usando makeStyles
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    // Remova o backgroundImage e outros estilos relacionados
+  },
+  paper: {
+    backgroundColor: theme.palette.login,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "55px 30px",
+    borderRadius: "12.5px",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  powered: {
+    color: "white",
+  },
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  const [user, setUser] = useState({ email: "", password: "" });
+  const { handleLogin } = useContext(AuthContext);
+
+  const handleChangeInput = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(user);
+  };
+
+  return (
+    <div className={`${classes.root} ReactToastify-BTGsrsBX`}> {/* Adicione a classe CSS aqui */}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <div>
+            <img style={{ margin: "0 auto", width: "100%" }} src={logo} alt="Whats" />
+          </div>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label={i18n.t("login.form.email")}
+              name="email"
+              value={user.email}
+              onChange={handleChangeInput}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label={i18n.t("login.form.password")}
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={handleChangeInput}
+              autoComplete="current-password"
+            />
+            <Grid container justifyContent="flex-end">
+              <Grid item xs={6} style={{ textAlign: "right" }}>
+                <Link component={RouterLink} to="/forgetpsw" variant="body2">
+                  Esqueceu sua senha?
+                </Link>
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {i18n.t("login.buttons.submit")}
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2" component={RouterLink} to="/signup">
+                  {i18n.t("login.buttons.register")}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
