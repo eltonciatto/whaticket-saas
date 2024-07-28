96b1c2adbe7fabe86f4f29d3247d2c583d97eace
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-07-28 17:53:01 -0300

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,23 +1,16 @@
 import "../bootstrap";
 
-const config = {
+module.exports = {
   define: {
     charset: "utf8mb4",
     collate: "utf8mb4_bin"
   },
   dialect: process.env.DB_DIALECT || "mysql",
-  timezone: process.env.DB_TIMEZONE || "-03:00", // Tornar o timezone configurável
+  timezone: "-03:00",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "3306", 10), // Garantir que o port é um número
+  port: process.env.DB_PORT || 3306,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   logging: process.env.DB_DEBUG === "true"
 };
-
-// Verificação das variáveis de ambiente essenciais
-if (!config.host || !config.database || !config.username || !config.password) {
-  throw new Error("Faltam variáveis de ambiente essenciais para a configuração do banco de dados.");
-}
-
-export default config;
