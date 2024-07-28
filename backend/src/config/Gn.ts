56a839a06ac573c60f0e952d67763ad459cad5e1
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-07-28 17:37:49 -0300

    Update Gn.ts

--- backend/src/config/Gn.ts
+++ backend/src/config/Gn.ts
@@ -1,12 +1,17 @@
 import path from "path";
 
+// Certifique-se de que as variáveis de ambiente necessárias estão definidas
+if (!process.env.GERENCIANET_CLIENT_ID || !process.env.GERENCIANET_CLIENT_SECRET || !process.env.GERENCIANET_PIX_CERT) {
+  throw new Error("Faltam variáveis de ambiente necessárias para a configuração do Gerencianet.");
+}
+
 const cert = path.join(
   __dirname,
   `../../certs/${process.env.GERENCIANET_PIX_CERT}.p12`
 );
 
-export = {
-  sandbox: false,
+export default {
+  sandbox: process.env.NODE_ENV === "development", // Alternar dinamicamente baseado no ambiente
   client_id: process.env.GERENCIANET_CLIENT_ID as string,
   client_secret: process.env.GERENCIANET_CLIENT_SECRET as string,
   pix_cert: cert
