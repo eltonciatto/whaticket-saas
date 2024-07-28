fc35e5603ea67b74a292b14e94aed836391e06c9
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-07-28 18:39:39 -0300

    Update cache.ts

--- backend/src/libs/cache.ts
+++ backend/src/libs/cache.ts
@@ -1,81 +1,17 @@
 import Redis from "ioredis";
-import { REDIS_URI_CONNECTION } from "../config/redis";
-import * as crypto from "crypto";
 
-// Configuração do cliente Redis com opções de gerenciamento de falhas
-const redis = new Redis(REDIS_URI_CONNECTION, {
-  connectTimeout: 10000, // Tempo limite de conexão: 10 segundos
-  maxRetriesPerRequest: 10, // Número máximo de tentativas de reenvio
-  retryStrategy: (times) => {
-    // Estrategia de reenvio exponencial
-    const delay = Math.min(times * 50, 2000); // Exponencialmente aumente o tempo de espera
-    return delay;
-  }
+// Configuração do cliente Redis com opções apropriadas para Upstash
+const client = new Redis({
+  port: 6379, // Porta do Redis
+  host: "intense-marmoset-49193.upstash.io", // Host do Redis
+  password: "AcApAAIjcDEyNzFhM2M3NWRlNzE0YmQ2YjVmNTVhNDgxNTA4MTA5MHAxMA", // Senha do Redis
+  tls: {} // Configuração para uso de SSL/TLS
 });
 
-// Função para criptografar parâmetros
-function encryptParams(params: any) {
-  const str = JSON.stringify(params);
-  return crypto.createHash("sha256").update(str).digest("base64");
-}
-
-// Função para definir valores no Redis com base nos parâmetros
-export async function setFromParams(
-  key: string,
-  params: any,
-  value: string,
-  option?: string,
-  optionValue?: string | number
-) {
-  const finalKey = `${key}:${encryptParams(params)}`;
-  try {
-    if (option !== undefined && optionValue !== undefined) {
-      // O ioredis não suporta diretamente as opções passadas como string
-      // A opção padrão será usada
-      await redis.set(finalKey, value, "EX", optionValue); // Usando "EX" para definir o tempo de expiração, se for numérico
-    } else {
-      await redis.set(finalKey, value);
-    }
-  } catch (error) {
-    console.error(`Erro ao definir o valor para a chave ${finalKey}:`, error);
-  }
-}
-
-// Função para obter valores do Redis com base nos parâmetros
-export async function getFromParams(key: string, params: any) {
-  const finalKey = `${key}:${encryptParams(params)}`;
-  try {
-    return await redis.get(finalKey);
-  } catch (error) {
-    console.error(`Erro ao obter o valor para a chave ${finalKey}:`, error);
-  }
-}
-
-// Função para deletar valores do Redis com base nos parâmetros
-export async function delFromParams(key: string, params: any) {
-  const finalKey = `${key}:${encryptParams(params)}`;
-  try {
-    await redis.del(finalKey);
-  } catch (error) {
-    console.error(`Erro ao deletar a chave ${finalKey}:`, error);
-  }
-}
-
 // Função para definir valores no Redis
-export async function set(
-  key: string,
-  value: string,
-  option?: string,
-  optionValue?: string | number
-) {
+export async function set(key: string, value: string) {
   try {
-    if (option !== undefined && optionValue !== undefined) {
-      // O ioredis não suporta diretamente as opções passadas como string
-      // A opção padrão será usada
-      await redis.set(key, value, "EX", optionValue); // Usando "EX" para definir o tempo de expiração, se for numérico
-    } else {
-      await redis.set(key, value);
-    }
+    await client.set(key, value);
   } catch (error) {
     console.error(`Erro ao definir o valor para a chave ${key}:`, error);
   }
@@ -84,27 +20,27 @@ export async function set(
 // Função para obter valores do Redis
 export async function get(key: string) {
   try {
-    return await redis.get(key);
+    return await client.get(key);
   } catch (error) {
     console.error(`Erro ao obter o valor para a chave ${key}:`, error);
-  }
-}
-
-// Função para obter chaves do Redis com base em um padrão
-export async function getKeys(pattern: string) {
-  try {
-    return await redis.keys(pattern);
-  } catch (error) {
-    console.error(`Erro ao obter chaves com padrão ${pattern}:`, error);
   }
 }
 
 // Função para deletar uma chave do Redis
 export async function del(key: string) {
   try {
-    await redis.del(key);
+    await client.del(key);
   } catch (error) {
     console.error(`Erro ao deletar a chave ${key}:`, error);
+  }
+}
+
+// Função para obter chaves do Redis com base em um padrão
+export async function getKeys(pattern: string) {
+  try {
+    return await client.keys(pattern);
+  } catch (error) {
+    console.error(`Erro ao obter chaves com padrão ${pattern}:`, error);
   }
 }
 
@@ -123,11 +59,8 @@ export async function delFromPattern(pattern: string) {
 // Exporta as funções para acesso externo
 export const cacheLayer = {
   set,
-  setFromParams,
   get,
-  getFromParams,
-  getKeys,
   del,
-  delFromParams,
+  getKeys,
   delFromPattern
 };
