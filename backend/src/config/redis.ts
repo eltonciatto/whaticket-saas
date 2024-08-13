f1668ca8a6a3ee2d00e23f99ac5db6503171c01c
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-13 13:43:03 -0300

    Update redis.ts

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,3 +1,3 @@
-export const REDIS_URI_CONNECTION = process.env.REDIS_URI || "redis://default:AcApAAIjcDEyNzFhM2M3NWRlNzE0YmQ2YjVmNTVhNDgxNTA4MTA5MHAxMA@intense-marmoset-49193.upstash.io:6379";
+export const REDIS_URI_CONNECTION = process.env.REDIS_URI || "redis://:wzev3MaNvwNX2tffYZGxpPic9jJvxHgLi7a1cs8HptZEvLQ3YrWHf7Ns4agQaYA9@154.38.191.117:38580/0";
 export const REDIS_OPT_LIMITER_MAX = process.env.REDIS_OPT_LIMITER_MAX || 1;
 export const REDIS_OPT_LIMITER_DURATION = process.env.REDIS_OPT_LIMITER_DURATION || 3000;
