aeca3ce29a1337628ad2d734ae9a521e6ad579d0
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-14 14:44:48 -0300

    Update toastError.js

--- frontend/src/errors/toastError.js
+++ frontend/src/errors/toastError.js
@@ -2,41 +2,30 @@ import { toast } from "react-toastify";
 import { i18n } from "../translate/i18n";
 import { isString } from 'lodash';
 
-const toastError = err => {
-	const errorMsg = err.response?.data?.error;
-	if (errorMsg) {
-		if (i18n.exists(`backendErrors.${errorMsg}`)) {
-			toast.error(i18n.t(`backendErrors.${errorMsg}`), {
-				toastId: errorMsg,
-				autoClose: 2000,
-				hideProgressBar: false,
-				closeOnClick: true,
-				pauseOnHover: false,
-				draggable: true,
-				progress: undefined,
-				theme: "light",
-			});
-			return
-		} else {
-			toast.error(errorMsg, {
-				toastId: errorMsg,
-				autoClose: 2000,
-				hideProgressBar: false,
-				closeOnClick: true,
-				pauseOnHover: false,
-				draggable: true,
-				progress: undefined,
-				theme: "light",
-			});
-			return
-		}
-	} if (isString(err)) {
-		toast.error(err);
-		return
-	} else {
-		toast.error("An error occurred!");
-		return
-	}
+const toastError = (err) => {
+  let errorMsg = '';
+
+  if (err.response && err.response.data && err.response.data.error) {
+    errorMsg = err.response.data.error;
+    if (i18n.exists(`backendErrors.${errorMsg}`)) {
+      errorMsg = i18n.t(`backendErrors.${errorMsg}`);
+    }
+  } else if (isString(err)) {
+    errorMsg = err;
+  } else {
+    errorMsg = "An error occurred!"; // Consider adding a unique code or identifier
+  }
+
+  toast.error(errorMsg, {
+    toastId: errorMsg, // Consider using a more unique ID if necessary
+    autoClose: 5000, // Adjusted autoClose for better user experience
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
 };
 
 export default toastError;
