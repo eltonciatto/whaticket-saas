13c548408d301026d1bb05e211ab68954169445b
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-16 12:22:31 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -10,8 +10,9 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(3250, () => {
-  console.log(`Server is running on port 3250`);
+const PORT = process.env.PORT || 3250;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   console.log(`Backend URL: ${REACT_APP_BACKEND_URL}`);
   console.log(`Close Tickets Auto Hours: ${REACT_APP_HOURS_CLOSE_TICKETS_AUTO}`);
 });
