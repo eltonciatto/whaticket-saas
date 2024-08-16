b045e68e710054886bc21e37c1de73538b803313
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-16 17:16:30 -0300

    Atualização de melhorias Title.js
    Melhorias :
    Desestruturação das Props: Usar desestruturação de props para tornar o código mais conciso.
    Definir PropTypes e defaultProps: Embora o componente seja simples, definir PropTypes e defaultProps ajuda a documentar o tipo de dado esperado e a garantir valores padrão.

--- frontend/src/pages/Dashboard/Title.js
+++ frontend/src/pages/Dashboard/Title.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
 
-const Title = props => {
+const Title = ({ children }) => {
 	return (
 		<Typography component="h2" variant="h6" color="primary" gutterBottom>
-			{props.children}
+			{children}
 		</Typography>
 	);
+};
+
+Title.propTypes = {
+	children: PropTypes.node.isRequired,
 };
 
 export default Title;
