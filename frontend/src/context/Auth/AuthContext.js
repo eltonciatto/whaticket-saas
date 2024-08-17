eda88877cca31b954dbe7208dc767465c90be244
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-16 23:09:04 -0300

    Update AuthContext.js

--- frontend/src/context/Auth/AuthContext.js
+++ frontend/src/context/Auth/AuthContext.js
@@ -1,20 +1,21 @@
-import React, { createContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import React, { createContext, useEffect } from "react";
+import Cookies from "js-cookie";
 import useAuth from "../../hooks/useAuth.js";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const { loading, user, isAuth, handleLogin, handleLogout } = useAuth();
-  const [sessionChecked, setSessionChecked] = useState(false); // Controle para evitar alertas duplicados
 
   useEffect(() => {
-    // Verifica se o usuário está autenticado e se a sessão já foi checada
-    if (!loading && !isAuth && !sessionChecked) {
-      toast.error("Sessão Expirada, por favor entre novamente");
-      setSessionChecked(true); // Marca como checado para não exibir novamente
+    // Verificar se o cookie de sessão já existe
+    const sessionCookie = Cookies.get("user_session");
+
+    // Se o usuário estiver logado e o cookie ainda não existir, definir o cookie
+    if (isAuth && !sessionCookie) {
+      Cookies.set("user_session", "active", { expires: 7 });
     }
-  }, [loading, isAuth, sessionChecked]);
+  }, [isAuth]);
 
   return (
     <AuthContext.Provider
