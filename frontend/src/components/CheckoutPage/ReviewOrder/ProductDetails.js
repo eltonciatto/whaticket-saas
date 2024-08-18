9b71628d9959f4b2f40846918dde90eb7e329e95
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-18 05:35:53 -0300

    Add brl corrente ProductDetails.js
    Adicionado o BRL como moeda corrente após a atualização de melhorias

--- frontend/src/components/CheckoutPage/ReviewOrder/ProductDetails.js
+++ frontend/src/components/CheckoutPage/ReviewOrder/ProductDetails.js
@@ -6,7 +6,7 @@ function ProductDetails({ products }) {
   const classes = useStyles();
 
   // Calcula o total dos produtos
-  const total = products.reduce((acc, product) => acc + parseFloat(product.price.replace('$', '')), 0);
+  const total = products.reduce((acc, product) => acc + parseFloat(product.price), 0);
 
   return (
     <List disablePadding>
@@ -14,14 +14,14 @@ function ProductDetails({ products }) {
         <ListItem className={classes.listItem} key={index}>
           <ListItemText primary={product.name} secondary={product.desc} />
           <Typography variant="body2">
-            {parseFloat(product.price).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+            {parseFloat(product.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
           </Typography>
         </ListItem>
       ))}
       <ListItem className={classes.listItem}>
         <ListItemText primary="Total" />
         <Typography variant="subtitle1" className={classes.total}>
-          {total.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+          {total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
         </Typography>
       </ListItem>
     </List>
