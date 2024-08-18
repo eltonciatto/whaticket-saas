6e1ec40641c2ce780190836070f060d24fdf3ce7
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-18 05:33:57 -0300

    Add melhorias ProductDetails.js
    Principais Ajustes:
    Valores Dinâmicos:
    
    Em vez de usar valores fixos, o componente agora espera uma lista de produtos passada como props. Isso torna o componente mais flexível e reutilizável.
    Cálculo Dinâmico do Total:
    
    O total é calculado dinamicamente com base nos produtos fornecidos.
    Formatação de Moeda:
    
    Os preços são formatados utilizando o método toLocaleString com o formato de moeda.

--- frontend/src/components/CheckoutPage/ReviewOrder/ProductDetails.js
+++ frontend/src/components/CheckoutPage/ReviewOrder/ProductDetails.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
 import useStyles from './styles';
 
-const products = [
-  { name: 'Product 1', desc: 'A nice thing', price: '$9.99' },
-  { name: 'Product 2', desc: 'Another thing', price: '$3.45' },
-  { name: 'Product 3', desc: 'Something else', price: '$6.51' },
-  { name: 'Product 4', desc: 'Best thing of all', price: '$14.11' },
-  { name: 'Shipping', desc: '', price: 'Free' }
-];
-
-function ProductDetails() {
+function ProductDetails({ products }) {
   const classes = useStyles();
+
+  // Calcula o total dos produtos
+  const total = products.reduce((acc, product) => acc + parseFloat(product.price.replace('$', '')), 0);
+
   return (
     <List disablePadding>
-      {products.map(product => (
-        <ListItem className={classes.listItem} key={product.name}>
+      {products.map((product, index) => (
+        <ListItem className={classes.listItem} key={index}>
           <ListItemText primary={product.name} secondary={product.desc} />
-          <Typography variant="body2">{product.price}</Typography>
+          <Typography variant="body2">
+            {parseFloat(product.price).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+          </Typography>
         </ListItem>
       ))}
       <ListItem className={classes.listItem}>
         <ListItemText primary="Total" />
         <Typography variant="subtitle1" className={classes.total}>
-          $34.06
+          {total.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
         </Typography>
       </ListItem>
     </List>
