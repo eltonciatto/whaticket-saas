4699abdb56360658393025c5005ac7145ce79114
Elton Ciatto <53355283+eltonciatto@users.noreply.github.com>
2024-08-19 19:24:00 -0300

    Add toast PaymentForm.js

--- frontend/src/components/CheckoutPage/Forms/PaymentForm.js
+++ frontend/src/components/CheckoutPage/Forms/PaymentForm.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
